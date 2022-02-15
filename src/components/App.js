2e6f9ac5395439f857248febe3318ec7c517dd8e
unknown <b160086@nitsikkim.ac.in>
2022-02-15 23:56:38 +0530

    adding navbar

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,55 @@ class App extends React.Component {
     console.log('posts');
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/482/482631.png"
+              alt="search-icon"
+            />
+            <input placeholder="search" />
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1644950203~hmac=0875a6207b627a88c1e5ab8153a23c35"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1644950203~hmac=0875a6207b627a88c1e5ab8153a23c35"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1644950203~hmac=0875a6207b627a88c1e5ab8153a23c35"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <PostList posts={posts} />
       </div>
     );
