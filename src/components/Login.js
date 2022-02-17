b8057dbd8272325f8ad96b77c6653e186c5fb7fc
unknown <b160086@nitsikkim.ac.in>
2022-02-17 20:29:15 +0530

    form handling uncontroller components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+
+  handleLoginButtonClick = (e) => {
+    e.preventDefault();
+    console.log(this.emailInputRef);
+    console.log(this.passwordInputRef);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header />">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={this.emailInputRef}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            ref={this.passwordInputRef}
+          />
         </div>
         <div className="field">
-          <button>Log in</button>
+          <button onClick={this.handleLoginButtonClick}>Log in</button>
         </div>
       </form>
     );
