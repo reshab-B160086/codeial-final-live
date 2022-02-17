3b449763890630acd87b9d8a758e4488d2593622
unknown <b160086@nitsikkim.ac.in>
2022-02-17 21:19:08 +0530

    adding user login reducer and action type

---
+++ src/reducers/auth.js
@@ -0,0 +1,37 @@
+import {
+  LOGIN_ERROR,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from '../actions/actionTypes';
+
+const initialAuthState = {
+  user: {},
+  error: null,
+  isLoggedIn: false,
+  iProgress: false,
+};
+
+export default function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        inProgress: false,
+        error: null,
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
