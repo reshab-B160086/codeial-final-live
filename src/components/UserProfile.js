5439c2e607ad6ad747a51ccece5cabaf8a2356a3
unknown <b160086@nitsikkim.ac.in>
2022-03-06 16:22:04 +0530

    added remove friend button

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,6 +4,14 @@ import { fetchUserProfile } from '../actions/profile';
 import profile from '../reducers/profile';
 
 class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: null,
+      error: null,
+    };
+  }
+
   componentDidMount() {
     const { match } = this.props;
     if (match.params.userId) {
@@ -11,6 +19,19 @@ class UserProfile extends React.Component {
       this.props.dispatch(fetchUserProfile(match.params.userId));
     }
   }
+
+  checkIfUserIsAFriend = () => {
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.tu_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
 
   render() {
     const {
@@ -24,6 +45,8 @@ class UserProfile extends React.Component {
     if (profile.inProgress) {
       return <h1>Loading !!!</h1>;
     }
+
+    const isUserAFriend = this.checkIfUserIsAFriend();
 
     return (
       <div className="settings">
@@ -39,16 +62,21 @@ class UserProfile extends React.Component {
           <div className="field-value">{user.email}</div>
         </div>
         <div className="btn-grp">
-          <button className="button save-btn">Add Friend</button>
+          {!isUserAFriend ? (
+            <button className="button save-btn">Add Friend</button>
+          ) : (
+            <button className="button save-btn">Remove Friend</button>
+          )}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ profile }) {
+function mapStateToProps({ profile, friends }) {
   return {
     profile,
+    friends,
   };
 }
 
